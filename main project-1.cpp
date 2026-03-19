19b94ba02d4daa87a3b1da020fe10ffc37d84b7c
MEHMET TATAR <mehmettatar5821@gmail.com>
2026-03-19 18:45:17 +0300

    Create main project-1.cpp

---
+++ main project-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+	
+	float average=0;
+	float numbers[3];
+cout<<"enter three numbers:";
+cin>>numbers[0]>>numbers[1]>>numbers[2];
+    average=(numbers[0]+numbers[1]+numbers[2])/3;
+cout<<"average: "<<average<<"\n";
+	
+	
+	
+	
+	
+	
+	return 0;
+}
