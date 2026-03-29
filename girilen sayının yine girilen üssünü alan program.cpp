cce2e24ff272cd8c37e6395ae01f1a3bf126bc9a
MEHMET TATAR <mehmettatar5821@gmail.com>
2026-03-29 19:45:02 +0300

    Create girilen sayının yine girilen üssünü alan program.cpp

---
+++ girilen sayının yine girilen üssünü alan program.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+ float exp(float x, float y);
+   
+int main( ) {
+	float base,result=0;
+	int exponent;
+	cout<<"Enter base and exponent values:";
+	cin>>base>>exponent;
+	result=exp(base,exponent);
+	cout<<"result:"<<result<<"\n";
+ 
+	return 0;
+}
+float exp(float x, float y){
+	float result=1;
+	int i;
+	if(x==0){
+		result=0;
+	}else {
+		if(y==0){
+			result=1;
+		}else if(y<0){
+			for(i=0;i<-y;i++)
+			result*=1/x;
+		}else{
+			for(i=0;i<y;i++)
+			result*=x;
+		}
+	}
+	return result;
+}
